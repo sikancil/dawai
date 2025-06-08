3c38ca3f4b23147cd044a7ff49582d9fd1417547
Arif Widianto <sikancil.r19@gmail.com>
2025-06-08 04:33:36 +0000

    Refactor: Introduce dawai-webservice, add example, remove shared
    - Create `@arifwidianto/dawai-webservice` for web-specific components.
    - Move `Body` decorator from `dawai-microservice` to `dawai-webservice`.
    - Add `main-showcase` example project to demonstrate new structure.
    - Remove `@arifwidianto/dawai-shared` package.
    - Update package configurations, dependencies, and `.gitignore`.

--- packages/dawai-shared/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-};
